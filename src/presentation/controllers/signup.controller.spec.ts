c19e56985697141262982e86458fac1a01cba863
Antonio Mata <hildeamp@gmail.com>
2020-08-08 19:57:26 -0600

    test: ensure SignupController returns 400 status code if no name is passed

---
+++ src/presentation/controllers/signup.controller.spec.ts
@@ -0,0 +1,16 @@
+import { SignupController } from './signup.controller';
+
+describe('Signup controller', () => {
+  it('should return 404 if no name is provided', () => {
+    const sut = new SignupController();
+    const httpRequest = {
+      body: {
+        email: 'name@email.com',
+        password: 'strong password',
+        passwordConfirmation: 'strong password',
+      },
+    };
+    const httpResponse = sut.handle(httpRequest);
+    expect(httpResponse.statusCode).toBe(400);
+  });
+});
