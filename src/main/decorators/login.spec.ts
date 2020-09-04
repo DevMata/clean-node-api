9b341efd6969adbdfa0926efeb2233dddbdbe356
Antonio Mata <hildeamp@gmail.com>
2020-09-03 22:48:46 -0600

    test: ensure LogControllerDecorator calls LogErrorRepository if controller returns a server error

--- src/main/decorators/login.spec.ts
+++ src/main/decorators/login.spec.ts
@@ -1,9 +1,12 @@
 import { LogControllerDecorator } from './login';
 import { Controller, HttpRequest, HttpResponse } from '../../presentation/protocols';
+import { serverError } from '../../presentation/helpers/http.helper';
+import { LogErrorRepository } from '../../data/protocols/log-error-repository';
 
 interface SutTypes {
   sut: LogControllerDecorator;
   controllerStub: Controller;
+  logErrorRepositoryStub: LogErrorRepository;
 }
 
 const makeController = (): Controller => {
@@ -20,13 +23,25 @@ const makeController = (): Controller => {
   return new ControllerStub();
 };
 
+const makeLogErrorRepository = (): LogErrorRepository => {
+  class LogErrorRepositoryStub implements LogErrorRepository {
+    async log(stack: string): Promise<void> {
+      return Promise.resolve();
+    }
+  }
+
+  return new LogErrorRepositoryStub();
+};
+
 const makeSut = (): SutTypes => {
   const controllerStub = makeController();
-  const sut = new LogControllerDecorator(controllerStub);
+  const logErrorRepositoryStub = makeLogErrorRepository();
+  const sut = new LogControllerDecorator(controllerStub, logErrorRepositoryStub);
 
   return {
     controllerStub,
     sut,
+    logErrorRepositoryStub,
   };
 };
 
@@ -61,5 +76,24 @@ describe('Login Controller Decorator', () => {
       statusCode: 200,
       body: { name: 'Antonio' },
     });
+  });
+
+  it('should call LogErrorRepository with correct error if controller returns a server error', async () => {
+    const { sut, controllerStub, logErrorRepositoryStub } = makeSut();
+    const fakeError = new Error();
+    fakeError.stack = 'any_stack';
+    const error = serverError(fakeError);
+    const logSpy = jest.spyOn(logErrorRepositoryStub, 'log');
+    jest.spyOn(controllerStub, 'handle').mockReturnValueOnce(Promise.resolve(error));
+    const httpRequest = {
+      body: {
+        name: 'any_name',
+        email: 'any_email@email.com',
+        password: 'any_password',
+        passwordConfirmation: 'any_password',
+      },
+    };
+    await sut.handle(httpRequest);
+    expect(logSpy).toHaveBeenCalledWith('any_stack');
   });
 });
