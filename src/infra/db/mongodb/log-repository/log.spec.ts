f05c50d614d1181ac9aebb46c1b0ca305ba367e0
Antonio Mata <hildeamp@gmail.com>
2020-09-10 21:42:02 -0600

    test: ensure LoginMongoRepository creates an error log on success

---
+++ src/infra/db/mongodb/log-repository/log.spec.ts
@@ -0,0 +1,30 @@
+import { MongoHelper } from '../helpers/mongo-helper';
+import { Collection } from 'mongodb';
+import { LoginMongoRepository } from './log';
+
+const { MONGO_URL } = process.env;
+
+describe('Log Mongo Repository', () => {
+  let errorCollection: Collection;
+
+  beforeEach(async () => {
+    errorCollection = await MongoHelper.getCollection('errors');
+    await errorCollection.deleteMany({});
+  });
+
+  beforeAll(async () => {
+    await MongoHelper.connect(MONGO_URL || ' ');
+  });
+
+  afterAll(async () => {
+    await MongoHelper.disconnect();
+  });
+
+  it('should create an error log on success', async () => {
+    const sut = new LoginMongoRepository();
+    await sut.logError('any_error');
+    const count = await errorCollection.countDocuments();
+
+    expect(count).toBe(1);
+  });
+});
